922cf04d8112877910076d3be95617bb18871c77
Yannick Eigensatz <yeigensatz@gmail.com>
2022-09-08 21:34:47 +0200

    Initial commit from Create Next App

--- next.config.js
+++ next.config.js
@@ -2,9 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+<<<<<<< HEAD
   serverRuntimeConfig: {
     PROJECT_ROOT: __dirname,
   },
 };
 
 module.exports = nextConfig;
+=======
+}
+
+module.exports = nextConfig
+>>>>>>> 7f00a0f (Initial commit from Create Next App)
